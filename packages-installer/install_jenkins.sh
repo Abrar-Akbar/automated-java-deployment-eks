336c0b9cd18f1ba7f0ed63d127a59e7eb54ee8b5
Abrar Akbar <abrarakbar623@gmail.com>
2024-05-10 11:42:01 +0000

    Add images scanner

--- packages-installer/install_jenkins.sh
+++ packages-installer/install_jenkins.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 sudo apt update -y
-sudo apt install fontconfig openjdk-17-jre
+sudo apt install fontconfig openjdk-17-jre -y
 java -version
 sudo apt update -y
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
@@ -9,7 +9,7 @@ echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 sudo systemctl status jenkins
